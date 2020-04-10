f3289fea58a3d349d6a1371ca09a6cd673aaee96
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-10 10:27:40 -0700

    completes stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -72,15 +72,25 @@ for c in cities:
 # Tucson: (32.1558,-110.8777)
 # Salt Lake City: (40.7774,-111.9301)
 
-# TODO Get latitude and longitude values from the user
+
+def normalize_floats(val1, val2):
+    val1, val2 = [float(val1), float(val2)]
+    return [val for val in ([val1, val2] if val1 < val2 else [val2, val1])]
 
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-    # within will hold the cities that fall within the specified region
-    within = []
-
-    # TODO Ensure that the lat and lon valuse are all floats
-    # Go through each city and check to see if it falls within
-    # the specified coordinates.
-
+    low_lat, high_lat = normalize_floats(lat1, lat2)
+    low_lon, high_lon = normalize_floats(lon1, lon2)
+    print('coords: ', low_lat, high_lat, low_lon, high_lon)
+    within = [city for city in cities if (city.lat >= low_lat and city.lat <= high_lat) and (
+        city.lon >= low_lon and city.lon <= high_lon)]
     return within
+
+
+lat1, lon1 = input(
+    'Enter a latitude value and a longitude value, separated by a comma: ').split(',')
+lat2, lon2 = input(
+    'Now enter two more values, just like above: ').split(',')
+bound_cities = cityreader_stretch(lat1, lon1, lat2, lon2, cities)
+for city in bound_cities:
+    print(city)
